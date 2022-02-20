c65b898fad947cbfc011bfc09ee6efed9aa0f374
stevescilar <stevemuambi@gmail.com>
2022-02-20 21:03:03 +0300

    imagiops

--- clients/models.py
+++ clients/models.py
@@ -1,3 +1,9 @@
 from django.db import models
-
+# from phonenumber_field.modelfields import phonenumber_field
+from phone_field import PhoneField
 # Create your models here.
+class Client(models.Model):
+    full_name = models.CharField(max_length=200)
+    # phone_number = PhoneNumberField(null=False, blank=False, unique=True)
+    phone_number = PhoneField(blank=True, help_text='Contact phone number')
+    
--- clients/views.py
+++ clients/views.py
@@ -1,5 +1,9 @@
-from django.shortcuts import render
+from django.http import HttpResponse
+# from django.shortcuts import render
 
 # Create your views here.
-def home(request):
-    return render (request,"home.html")
+# def home(request):
+#     return render (request,"home.html")
+
+def index(request):
+    return HttpResponse("here again")
