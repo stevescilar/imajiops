902b607c7df56a064370994db6389043dfcbd8f1
stevescilar <stevemuambi@gmail.com>
2022-02-22 23:29:39 +0300

    imajiops

--- clients/views.py
+++ clients/views.py
@@ -1,9 +1,9 @@
-from django.http import HttpResponse
-# from django.shortcuts import render
+# from django.http import HttpResponse
+from django.shortcuts import render
 
 # Create your views here.
 # def home(request):
 #     return render (request,"home.html")
 
 def index(request):
-    return HttpResponse("here again")
+    return render (request,'home.html')
